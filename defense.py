cb3072702c8500f91e679cdfa252cf662ebdcbcf
dallaal <dallaal@D01RI1301200.ies.jrc.it>
2015-07-10 14:00:49 +0200

    d

--- defense.py
+++ defense.py
@@ -11,9 +11,9 @@ ogame = OGame(data['uni'], data['user'], data['password'], data['server'])
 planets = [33759952, 33763525]
 
 for planet in planets:
-	ogame.build(planet, (Defense['PlasmaTurret'], 10))
+	ogame.build(planet, (Defense['PlasmaTurret'], 50))
 	ogame.build(planet, (Defense['GaussCannon'], 20))
 	ogame.build(planet, (Defense['IonCannon'], 20))
 	ogame.build(planet, (Defense['HeavyLaser'], 100))
-	ogame.build(planet, (Defense['LightLaser'], 200))
-	ogame.build(planet, (Defense['RocketLauncher'], 1500))
+        #ogame.build(planet, (Defense['LightLaser'], 200))
+	#ogame.build(planet, (Defense['RocketLauncher'], 1500))
