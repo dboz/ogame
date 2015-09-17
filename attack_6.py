650c1f4dfb37dcd64bb0242db22fc3c432d9cb9c
dallaal <dallaal@D01RI1301200.ies.jrc.it>
2015-09-17 15:24:33 +0200

    attack 6

--- attack_6.py
+++ attack_6.py
@@ -14,12 +14,12 @@ targets = [ {'galaxy': 1, 'system': 13, 'position': 7},
             {'galaxy': 1, 'system': 13, 'position': 8},
             {'galaxy': 1, 'system': 11, 'position': 4},
             {'galaxy': 1, 'system': 11, 'position': 11},
+            {'galaxy': 1, 'system': 11, 'position': 4},
+            {'galaxy': 1, 'system': 11, 'position': 5},
+            {'galaxy': 1, 'system': 11, 'position': 11},
             {'galaxy': 1, 'system': 11, 'position': 13},
-            {'galaxy': 1, 'system': 10, 'position': 8},
-            {'galaxy': 1, 'system': 10, 'position': 13},
-            {'galaxy': 1, 'system': 9, 'position': 8},
-            {'galaxy': 1, 'system': 8, 'position': 9},
-            {'galaxy': 1, 'system': 8, 'position': 6},]
+            {'galaxy': 1, 'system': 10, 'position': 12},
+            {'galaxy': 1, 'system': 10, 'position': 7},]
 
 speed = Speed['100%']
 mission = Missions['Attack']
