7afb75c5773a1619e0874c0abae90ed8dd174d9f
dallaal <dallaal@D01RI1301200.ies.jrc.it>
2015-09-17 14:48:52 +0200

    attack 2

--- attack_2.py
+++ attack_2.py
@@ -10,16 +10,16 @@ ogame = OGame(data['uni'], data['user'], data['password'], data['server'])
 
 
 planet = 33759952
-targets = [ {'galaxy': 1, 'system': 36, 'position': 5},
-            {'galaxy': 1, 'system': 38, 'position': 5},
-            {'galaxy': 1, 'system': 38, 'position': 9},
-            {'galaxy': 1, 'system': 27, 'position': 11},
-            {'galaxy': 1, 'system': 39, 'position': 11},
-            {'galaxy': 1, 'system': 40, 'position': 12},
-            {'galaxy': 1, 'system': 43, 'position': 6},
-            {'galaxy': 1, 'system': 44, 'position': 5},
-            {'galaxy': 1, 'system': 45, 'position': 4},]
-            #{'galaxy': 1, 'system': 47, 'position': 11},]
+targets = [ {'galaxy': 1, 'system': 31, 'position': 4},
+            {'galaxy': 1, 'system': 31, 'position': 6},
+            {'galaxy': 1, 'system': 31, 'position': 7},
+            {'galaxy': 1, 'system': 30, 'position': 4},
+            {'galaxy': 1, 'system': 30, 'position': 8},
+            {'galaxy': 1, 'system': 30, 'position': 9},
+            {'galaxy': 1, 'system': 30, 'position': 12},
+            {'galaxy': 1, 'system': 29, 'position': 5},
+            {'galaxy': 1, 'system': 29, 'position': 7},
+            {'galaxy': 1, 'system': 29, 'position': 13},]
 
 speed = Speed['100%']
 mission = Missions['Attack']
