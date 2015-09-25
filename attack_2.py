9ee2e380ec655dc26074ff46fd01b2f5c33f6be4
dallaal <dallaal@D01RI1301200.ies.jrc.it>
2015-09-25 15:28:16 +0200

    bug fix

--- attack_2.py
+++ attack_2.py
@@ -1,6 +1,7 @@
 import json
 from ogame import OGame
 from ogame.constants import Ships, Speed, Missions, Buildings, Research, Defense
+import datetime
 
 with open('/root/ogame/config.json') as data_file:    
 	data = json.load(data_file)
@@ -9,8 +10,13 @@ with open('/root/ogame/config.json') as data_file:
 ogame = OGame(data['uni'], data['user'], data['password'], data['server'])
 
 
+now = datetime.datetime.now()
+
+print "Starting attack 2"
+print str(now)
+
 planet = 33759952
-targets = [ {'galaxy': 1, 'system': 31, 'position': 4},
+targets = [ {'galaxy': 1, 'system': 31, 'position': 4}, 
             {'galaxy': 1, 'system': 31, 'position': 6},
             {'galaxy': 1, 'system': 31, 'position': 7},
             {'galaxy': 1, 'system': 30, 'position': 4},
@@ -25,8 +31,8 @@ speed = Speed['100%']
 mission = Missions['Attack']
 #2 hours
 for target in targets:
-    ogame.send_fleet(planet, [(Ships['SmallCargo'], 50)], speed, target, mission, {})
-
-
-
-		
+    try:
+        ogame.send_fleet(planet, [(Ships['SmallCargo'], 50)], speed, target, mission, {})
+    except Exception, e:
+            print 'Unable to perform target %s: %s: %s' % (target['system'], target['position'], str(e))
+            
